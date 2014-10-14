d7dd9aa670bfa280508a220b8a6e4d1efc9e5d96
mattashii <boekewurm@gmail.com>
2014-10-14 12:11:04 +0200

    work on the item things

--- Main.py
+++ Main.py
@@ -1,22 +1,22 @@
 from tkinter import *
 root = Tk(screenName="Name", baseName="Name", className="Name")
 root.resizable = TRUE
-screenWidth = 1024
-screenHeight = 720
+SCREENWIDTH = 1024
+SCREENHEIGHT = 720
 #test = Frame(root, bg="red", width=100, height=100).pack(side=LEFT)
 
 #frame = Frame(root, width=screenWidth, height=screenHeight, bg="yellow").grid()
 #textFrame = Frame(root, width=screenWidth/3, height=screenHeight, bg="", colormap="new").pack()
 #buttonFrame = Frame(frame).pack(anchor=E)
 
-boughtItemList = Frame(root, width=(screenWidth / 3), height=screenHeight, bg="red")
+boughtItemList = Frame(root, width=(SCREENWIDTH / 3), height=SCREENHEIGHT, bg="red")
 boughtItemList.grid(column=0, row=0, rowspan=2)
-sellingItemList = Frame(root, width=(screenWidth / 3 * 2), height=(screenHeight - 100), bg="green")
+sellingItemList = Frame(root, width=(SCREENWIDTH / 3 * 2), height=(SCREENHEIGHT - 100), bg="green")
 sellingItemList.grid(column=1, row=0, columnspan=2)
-calculator = Frame(root, width=(screenWidth / 3 * 2), height=100, bg="blue")
+calculator = Frame(root, width=(SCREENWIDTH / 3 * 2), height=100, bg="blue")
 calculator.grid(column=1, row=1, columnspan=2)
 
-itemList = Text(boughtItemList, width=screenWidth/3, height=(2*screenHeight/3))
+itemList = Text(boughtItemList, width=SCREENWIDTH/3, height=(2*SCREENHEIGHT/3))
 """
 button1 = Button(boughtItemList, text="button1", borderwidth=10, relief=FLAT)
 button1.pack()
@@ -27,7 +27,7 @@ button3.grid(column=0, row=0)
 """
 #itemList = Text(frame).grid(column=0, row=1)
 
-bottomBar = Frame(root, width=screenWidth, height=40, bg="black")
+bottomBar = Frame(root, width=SCREENWIDTH, height=40, bg="black")
 bottomBar.grid(column=0, row=2, columnspan=3)
 status = Label(bottomBar, text="Waiting for action", bd=1, relief=SUNKEN).pack(fill=Y)
 
--- Terminal/Items/Item.py
+++ Terminal/Items/Item.py
@@ -1,6 +1,7 @@
 __author__ = 'Matthias'
 
 from Terminal.Items.ItemStock import ItemStock
+from Tkinter import *
 
 
 class Item:
@@ -8,9 +9,10 @@ class Item:
         self.name = name
         self.price = price
         self.image_link = image_link
+        self.tile = self.create_item_tile()
 
     def set_image(self, link: str):
-        self.image = link
+        self.image_link = link
 
     def set_price(self, price: float):
         self.price = price
@@ -29,3 +31,23 @@ class Item:
 
     def get_item_stack(self, amount: int):
         return ItemStock(self, amount)
+
+    def get_item_tile(self):
+        return self.tile
+
+    def create_item_tile(self, parent: Widget=None):
+        tile = Frame(parent, bg="black")
+        tile.pack_propagate(False)
+        if self.image_link:
+            img = PhotoImage(file=self.image_link)
+            img_lbl = Label(tile, image=img, height=50, width=50)
+            img_lbl.img = img
+            img_lbl.grid(column=0, row=0, columnspan=3)
+        else:
+            img_lbl = Label(tile, height=50, width=50, color="gray")
+            img_lbl.grid(column=0, row=0, columnspan=3)
+        name_label = Label(tile, text=self.name, font=(Arial, 12))
+        name_label.grid(column=0, row=1, columnspan=2)
+        price_label = Label(tile, text=str(self.price), font=(Arial, 14, bold))
+        price_label.grid(column=2, row=1)
+        return tile
