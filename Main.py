4c0a02fb35aca851d6d41703d27777b3c5e2fee3
Boschma <reneboschma@hotmail.com>
2014-10-14 11:24:50 +0200

    eerste commit test

---
+++ Main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+root = Tk(screenName="Name", baseName="Name", className="Name")
+root.resizable = TRUE
+screenWidth = 1024
+screenHeight = 720
+#test = Frame(root, bg="red", width=100, height=100).pack(side=LEFT)
+
+#frame = Frame(root, width=screenWidth, height=screenHeight, bg="yellow").grid()
+#textFrame = Frame(root, width=screenWidth/3, height=screenHeight, bg="", colormap="new").pack()
+#buttonFrame = Frame(frame).pack(anchor=E)
+
+boughtItemList = Frame(root, width=(screenWidth / 3), height=screenHeight, bg="red")
+boughtItemList.grid(column=0, row=0, rowspan=2)
+sellingItemList = Frame(root, width=(screenWidth / 3 * 2), height=(screenHeight - 100), bg="green")
+sellingItemList.grid(column=1, row=0, columnspan=2)
+calculator = Frame(root, width=(screenWidth / 3 * 2), height=100, bg="blue")
+calculator.grid(column=1, row=1, columnspan=2)
+
+itemList = Text(boughtItemList, width=screenWidth/3, height=(2*screenHeigth/3)
+"""
+button1 = Button(boughtItemList, text="button1", borderwidth=10, relief=FLAT)
+button1.pack()
+button2 = Button(sellingItemList, text="button2", borderwidth=10, relief=FLAT)
+button2.grid(column=0, row=0)
+button3 = Button(calculator, text="button3", borderwidth=10, relief=FLAT)
+button3.grid(column=0, row=0)
+"""
+#itemList = Text(frame).grid(column=0, row=1)
+
+bottomBar = Frame(root, width=screenWidth, height=40, bg="black")
+bottomBar.grid(column=0, row=2, columnspan=3)
+status = Label(bottomBar, text="Waiting for action", bd=1, relief=SUNKEN).pack(fill=Y)
+
+root.mainloop()
