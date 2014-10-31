51a2dcafe61b3b7d20519c6bfae7434e3cf7dc85
Cotix <Noahgoldsmid@live.nl>
2014-10-31 21:21:30 +0100

    Update connection.py

--- connection.py
+++ connection.py
@@ -8,7 +8,7 @@ class serverConnection:
         self.RSAServer = RSA.importKey(b'-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCniHbdv+321GAunHmPZ03DtFDL\nQKu26VyDrUsNYbZgLcnGhm6/itLRMuKaGhCnin55om4JR+awPQegUgxwJBTvk9rL\ng2Ax0UdYw2LG4S2YnTjKsQ8qfqwroTkRQNddBfj/VDNTrk1YF+/32uDvSQZlJhZo\nlq6BMu8VpDo0clid9wIDAQAB\n-----END PUBLIC KEY-----')
         self.RSALocal = RSA.importKey(open("local.key", "r").read())
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(("localhost", 3636))
+        self.sock.connect(("nitrogen.student.utwente.nl", 3636))
         data = self.RSALocal.publickey().exportKey("PEM")
         self.sock.sendall(data)
 
